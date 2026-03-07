78a4db888839a56f5fe173a514d0dc23724bbdd5
Wix Remote Machine <support@wix.com>
2026-03-07 07:00:00 +0000

    Update main image to ballerina

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ import { BaseCrudService } from '@/integrations';
 import { playClickSound } from '@/lib/click-sound';
 
 export default function HeroSection() {
-  const [heroImage, setHeroImage] = useState('https://static.wixstatic.com/media/e9d727_1863a0ceed024925b98e4c777d55b483~mv2.png?originWidth=1600&originHeight=1152');
+  const [heroImage, setHeroImage] = useState('https://static.wixstatic.com/media/e9d727_e512f95c1c554c68831b248aa4b50f26~mv2.png?originWidth=1600&originHeight=1152');
   const [videoUrl, setVideoUrl] = useState<string | null>(null);
 
   useEffect(() => {
