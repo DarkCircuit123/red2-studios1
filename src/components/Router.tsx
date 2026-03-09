a8eb7b4dbf52ff74e62fd39c2a9512ed7c4a7e5b
Wix Remote Machine <support@wix.com>
2026-03-09 23:36:07 +0000

    Force Static Router Import

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { MemberProvider } from '@/integrations';
 import { createBrowserRouter, RouterProvider, Navigate, Outlet } from 'react-router-dom';
 import { ScrollToTop } from '@/lib/scroll-to-top';
@@ -7,7 +7,6 @@ import { MemberProtectedRoute } from '@/components/ui/member-protected-route';
 import SEOOptimizer from '@/components/SEOOptimizer';
 import { useContentProtection } from '@/hooks/useContentProtection';
 import { AppInitializer } from '@/components/AppInitializer';
-import { LoadingSpinner } from '@/components/ui/loading-spinner';
 
 // Import all pages with explicit static imports
 import HomePage from './pages/HomePage';
@@ -24,22 +23,14 @@ import ChatPage from './pages/ChatPage';
 import Red2TerminalPage from './pages/Red2TerminalPage';
 
 function Layout() {
+  useContentProtection(true);
   return (
-    <Suspense fallback={<LoadingSpinner />}>
+    <>
       <AppInitializer />
       <SEOOptimizer />
       <ScrollToTop />
-      <LayoutContent />
-    </Suspense>
-  );
-}
-
-function LayoutContent() {
-  useContentProtection(true);
-  return (
-    <Suspense fallback={<LoadingSpinner />}>
       <Outlet />
-    </Suspense>
+    </>
   );
 }
 
@@ -113,12 +104,10 @@ const router = createBrowserRouter([
 
 function AppRouter() {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <MemberProvider>
-        <RouterProvider router={router} />
-      </MemberProvider>
-    </Suspense>
+    <MemberProvider>
+      <RouterProvider router={router} />
+    </MemberProvider>
   );
 }
 
-export default React.memo(AppRouter);
+export default AppRouter;
