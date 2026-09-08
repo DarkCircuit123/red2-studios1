14118b7cf096afda23ef9e617575b2dcbb53025b
Wix Remote Machine <support@wix.com>
2026-09-08 04:58:55 +0000

    Reverting last change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -173,7 +173,7 @@ export default function Header() {
             transition: color 300ms ease;
           }
           .logo-wrap:hover .logo-2-front {
-            color: #8C2F2F;
+            color: #A31621;
           }
           .logo-2-back {
             display: none;
